589a5d6fcb821815e7390e91d953e1808892347b
Dinh Trung <dodinhtrung2004@gmail.com>
2024-09-17 14:52:00 +0700

    cập nhật product

--- src/main/java/com/nhom7/ecommercebackend/controller/ProductController.java
+++ src/main/java/com/nhom7/ecommercebackend/controller/ProductController.java
@@ -1,14 +1,16 @@
 package com.nhom7.ecommercebackend.controller;
 
 import com.nhom7.ecommercebackend.model.Product;
+import com.nhom7.ecommercebackend.request.ProductDTO;
+import com.nhom7.ecommercebackend.response.ApiResponse;
 import com.nhom7.ecommercebackend.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.net.HttpURLConnection.HTTP_OK;
 
 @RestController
 @RequestMapping("${api.prefix}/products")
@@ -17,24 +19,71 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public ApiResponse getAllProducts() {
+        List<ProductDTO> productDTOList = productService.getAllProducts()
+                .stream()
+                .map(this::toProductDTO)
+                .collect(Collectors.toList());
+        return ApiResponse.builder()
+                .data(productDTOList)
+                .message("Fetched all products successfully!")
+                .status(HTTP_OK)
+                .build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        Optional<Product> product = Optional.ofNullable(productService.getProductById(id));
-        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public ApiResponse getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ApiResponse.builder()
+                    .data(toProductDTO(product))
+                    .message("Fetched product successfully!")
+                    .status(HTTP_OK)
+                    .build();
+        } else {
+            return ApiResponse.builder()
+                    .message("Product not found!")
+                    .status(HTTP_OK)
+                    .build();
+        }
     }
 
     @PostMapping
-    public Product createProduct(@RequestBody Product product) {
-        return productService.saveProduct(product);
+    public ApiResponse createProduct(@RequestBody ProductDTO productDTO) {
+        Product newProduct = productService.saveProduct(toProductEntity(productDTO));
+        return ApiResponse.builder()
+                .data(toProductDTO(newProduct))
+                .message("Create Product successfully!")
+                .status(HTTP_OK)
+                .build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+    public ApiResponse deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
-        return ResponseEntity.noContent().build();
+        return ApiResponse.builder()
+                .message("Product deleted successfully!")
+                .status(HTTP_OK)
+                .build();
+    }
+
+    // Converts Product entity to ProductDTO
+    private ProductDTO toProductDTO(Product product) {
+        return ProductDTO.builder()
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .thumbnail(product.getThumbnail())
+                .build();
+    }
+
+    // Converts ProductDTO to Product entity
+    private Product toProductEntity(ProductDTO productDTO) {
+        Product product = new Product();
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setThumbnail(productDTO.getThumbnail());
+        return product;
     }
 }
--- src/main/java/com/nhom7/ecommercebackend/request/ProductDTO.java
+++ src/main/java/com/nhom7/ecommercebackend/request/ProductDTO.java
@@ -1,12 +1,17 @@
 package com.nhom7.ecommercebackend.request;
 
+import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
+@Builder
 public class ProductDTO {
+    @Range(min = 3, message = "Product name must have at least 3 characters")
     private String name;
+    @Range(min = 3, message = "Product description must have at least 3 characters")
     private String description;
     private float price;
     private String thumbnail;
-    private Long categoryId; // Category ID for associating product with a category
+    private Long categoryId;
 }
